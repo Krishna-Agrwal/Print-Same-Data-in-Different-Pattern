9526dbdef53e8521b9ddf45534ae8b8149e5aba0
Krishna Agarwal <codekrishna86@gmail.com>
2024-05-30 20:13:57 +0530

    Add files via upload

---
+++ Print Same Data in Different Pattern.py
@@ -0,0 +1,20 @@
+#Try to print the following string: 
+#Example 1 : 
+#if n = 5
+#print = 12345
+
+n = int(input())
+for i in range(1,n+1):
+  print(i,end="")
+
+#Example 2: 
+#if n = 5
+#print = 
+#1
+#2
+#3
+#4
+#5
+#n = int(input())
+for i in range(1,n+1):
+  print(i)
